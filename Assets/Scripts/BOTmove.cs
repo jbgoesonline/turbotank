cc9837e17d9f2be1cea114a950b153a36e141357
Srikanth <srikanth817@gmail.com>
2021-01-07 21:46:16 -0700

    Bot patrols if there are waypoints

--- Assets/Scripts/BOTmove.cs
+++ Assets/Scripts/BOTmove.cs
@@ -18,12 +18,23 @@ public class BOTmove : MonoBehaviour
     public int currentHealth = 100;
     public int damageValue = 20;
      Vector3 MousePos;
+     int wp = 0;
+     List <GameObject> waypoints = new List<GameObject>();
 
     // Start is called before the first frame update
        void Start()
     {
         //Get and store a reference to the Rigidbody2D component so that we can access it.
         rb = GetComponent<Rigidbody2D> ();
+        
+        for(int i = 0; i<4; i++){
+            if(GameObject.Find("Waypoint"+i))
+            {
+                Debug.Log("Found");
+                waypoints.Add(GameObject.Find("Waypoint"+i));
+            }
+            //Debug.Log("Waypoint" + i);
+        }
         
         //heatlh bar init
         currentHealth = maxHealth;
@@ -35,14 +46,19 @@ public class BOTmove : MonoBehaviour
 
     private void Update(){
         //rotate bot towards mouse
-        MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(waypoints.Count == 0){
+            MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }else{
+        
+            MousePos = waypoints[wp].transform.position;
+        }
         Vector2 lookDir = MousePos-transform.position;
         if(lookDir.magnitude>1){
             float angle = Mathf.Atan2(lookDir.y, lookDir.x)*Mathf.Rad2Deg;
             Quaternion angle_q =  Quaternion.Euler (0f, 0f, angle);
             Quaternion init = transform.rotation;
             //while()
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, angle_q, Time.deltaTime * 50);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, angle_q, Time.deltaTime * 100);
             //move forwards
             float xv = (float) Math.Cos(rb.rotation*(Math.PI/180));
             float yv = (float) Math.Sin(rb.rotation*(Math.PI/180));
@@ -51,6 +67,10 @@ public class BOTmove : MonoBehaviour
             rb.velocity = movement;
         }else{
             rb.velocity = new Vector2 (0, 0);
+            wp +=1;
+            if(wp==waypoints.Count){
+                wp = 0;
+            }
         }
     }
 
